2fbc5027d6d113ebb62a757e927e7f1803002ae7
Thoelen Bilal (s140178) <bilal.thoelen@student.ap.be>
2024-05-29 02:21:52 +0000

    Mogged

--- database.ts
+++ database.ts
@@ -1,83 +1,90 @@
-import {Collection, MongoClient } from "mongodb";
+import { Collection, MongoClient } from "mongodb";
 import { User, Datum, Datum2 } from "./interfaces";
 import bcrypt from "bcrypt";
 import dotenv from "dotenv";
 dotenv.config();
 
-
-export const uri = "mongodb+srv://TiltedTowers:TiltedTowersBOM@cluster0.iwefcqe.mongodb.net/";
+export const uri =
+  "mongodb+srv://TiltedTowers:TiltedTowersBOM@cluster0.iwefcqe.mongodb.net/";
 export const client = new MongoClient(uri);
 
-export const usersCollection: Collection<User> = client.db("users").collection<User>("usersCollection");
-export const charactersCollection: Collection<Datum2> = client.db("characters").collection<Datum2>("charactersCollection");
+export const usersCollection: Collection<User> = client
+  .db("users")
+  .collection<User>("usersCollection");
+export const charactersCollection: Collection<Datum2> = client
+  .db("characters")
+  .collection<Datum2>("charactersCollection");
 
-
-const saltRounds : number = 10;
+const saltRounds: number = 10;
 
 async function createInitialUser() {
-    if (await usersCollection.countDocuments() > 0) {
-        return;
-    }
-    const email: string | undefined = process.env.ADMIN_EMAIL;
-    const password: string | undefined = process.env.ADMIN_PASSWORD;
-    if (email === undefined || password === undefined) {
-        throw new Error("ADMIN_EMAIL and ADMIN_PASSWORD must be set in environment");
-    }
+  if ((await usersCollection.countDocuments()) > 0) {
+    return;
+  }
+  const email: string | undefined = process.env.ADMIN_EMAIL;
+  const password: string | undefined = process.env.ADMIN_PASSWORD;
+  if (email === undefined || password === undefined) {
+    throw new Error(
+      "ADMIN_EMAIL and ADMIN_PASSWORD must be set in environment"
+    );
+  }
 
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-    await usersCollection.insertOne({
-        username: "admin",
-        email: email,
-        password: hashedPassword,
-        game: "Fortnite",
-    });
+  await usersCollection.insertOne({
+    username: "admin",
+    email: email,
+    password: hashedPassword,
+    game: "Fortnite",
+  });
 }
 
 export async function login(email: string, password: string) {
-    if (email === "" || password === "") {
-        throw new Error("Email en wachtwoord zijn vereist.");
-    }
-    const user: User | null = await usersCollection.findOne<User>({ email: email });
-    if (user) {
-        const passwordMatch = await bcrypt.compare(password, user.password);
-        if (passwordMatch) {
-            return user;
-        } else {
-            throw new Error("Verkeerd wachtwoord!");
-        }
+  if (email === "" || password === "") {
+    throw new Error("Email en wachtwoord zijn vereist.");
+  }
+  const user: User | null = await usersCollection.findOne<User>({
+    email: email,
+  });
+  if (user) {
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (passwordMatch) {
+      return user;
     } else {
-        throw new Error("Gebruiker niet gevonden, maak een account aan.");
+      throw new Error("Verkeerd wachtwoord!");
     }
+  } else {
+    throw new Error("Gebruiker niet gevonden, maak een account aan.");
+  }
 }
 
 async function exit() {
-    try {
-        await client.close();
-        console.log("Disconnected from database");
-    } catch (error) {
-        console.error(error);
-    }
-    process.exit(0);
+  try {
+    await client.close();
+    console.log("Disconnected from database");
+  } catch (error) {
+    console.error(error);
+  }
+  process.exit(0);
 }
 
 export async function connect() {
-    try {
-        await client.connect();
-        await createInitialUser();
-        console.log("Connected to database");
-        process.on("SIGINT", exit);
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    await client.connect();
+    await createInitialUser();
+    console.log("Connected to database");
+    process.on("SIGINT", exit);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export async function loadCharacters() {
-    try {
-        const characters = await charactersCollection.find().toArray();
-        return characters;
-    } catch (error) {
-        console.error("Error loading characters:", error);
-        throw error;
-    }
+  try {
+    const characters = await charactersCollection.find().toArray();
+    return characters;
+  } catch (error) {
+    console.error("Error loading characters:", error);
+    throw error;
+  }
 }
--- routes/loginRouter.ts
+++ routes/loginRouter.ts
@@ -7,16 +7,16 @@ import session from "../session";
 import { secureMiddleware } from "../session";
 import { loadCharacters } from "../database";
 import { User } from "../interfaces";
-
-
-
+ 
+ 
+ 
 export function loginRouter() {
     const router = express.Router();
-
+ 
     router.get("/login", async (req, res) => {
         res.render("login", { errorMessage: null});
     });
-
+ 
     router.post("/login", async (req, res) => {
         const email: string = req.body.email;
         const password: string = req.body.password;
@@ -33,28 +33,28 @@ export function loginRouter() {
             res.redirect("/login");        
         }
     });
-    
-    
+   
+   
     router.get('/landing-page', secureMiddleware, (req, res) => {
         res.render('landing-page', { user: req.session.user });
     });
-
+ 
     router.get('/homepage_fortnite', secureMiddleware, (req, res) => {
         res.render('homepage_fortnite', { user: req.session.user });
     });
-
+ 
     router.get('/blacklist_detail', secureMiddleware, (req, res) => {
         res.render('blacklist_detail', { user: req.session.user });
     });
-
+ 
     router.get('/blacklisted_characters', secureMiddleware, (req, res) => {
         res.render('blacklisted_characters', { user: req.session.user });
     });
-
+ 
     router.get('/character_detail', secureMiddleware, (req, res) => {
         res.render('character_detail', { user: req.session.user });
     });
-
+ 
     router.get('/characters', secureMiddleware, async (req, res) => {
         try {
             const characters = await loadCharacters();
@@ -63,37 +63,37 @@ export function loginRouter() {
             res.status(500).send("Error loading characters");
         }
     });
-
+ 
     router.get('/favorite_characters', secureMiddleware, (req, res) => {
         res.render('favorite_characters', { user: req.session.user });
     });
-
+ 
     router.get('/favorite_detail', secureMiddleware, (req, res) => {
         res.render('favorite_detail', { user: req.session.user });
     });
-
+ 
     router.get('/user-profile', secureMiddleware, (req, res) => {
         res.render('user-profile', { user: req.session.user });
     });
-
+ 
     router.get('/registration', async (req, res) => {
         res.render('registration', { user: req.session.user });
     });
-
+ 
     router.post("/logout", secureMiddleware, async (req, res) => {
         try {
             req.session.destroy((err) => {
                 if (err) {
                     console.error("Fout bij het vernietigen van de sessie:", err);
                 }
-                res.redirect("/login"); 
+                res.redirect("/login");
             });
         } catch (error: any) {
             console.error("Fout bij het uitloggen:", error);
-            res.redirect("/"); 
+            res.redirect("/");
         }
     });
-    
-
+   
+ 
     return router;
 }
